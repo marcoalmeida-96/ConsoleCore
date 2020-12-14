63b1a650556806cad9c955efad8a485fec7e689e
Marco Rodrigues Almeida <marcoalmeida.96@hotmail.com>
2020-12-14 19:10:38 +0000

    First Commit

---
+++ GameTOP/JogoFODA.cs
@@ -0,0 +1,30 @@
+using GameTOP.Interface;
+
+namespace GameTOP
+{
+    public class JogoFODA
+    {
+        private readonly iJogador _jogadorA;
+
+        private readonly iJogador _jogadorB;
+
+        public JogoFODA(iJogador jogadorA, iJogador jogadorB)
+        {
+            _jogadorA = jogadorA;
+            _jogadorB = jogadorB;
+        }
+
+        public void IniciarJogo()
+        {
+            System.Console.Write(_jogadorA.Corre());
+            System.Console.Write(_jogadorA.Passe());
+            System.Console.Write(_jogadorA.Chuta());
+
+            System.Console.Write("\n Proximo jogador \n");
+
+            System.Console.Write(_jogadorB.Corre());
+            System.Console.Write(_jogadorB.Passe());
+            System.Console.Write(_jogadorB.Chuta());
+        }
+    }
+}
